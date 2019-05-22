c15c9a050f30126e2cdba24bcd11056651f8d6b8
chekos <chekos.data@gmail.com>
2019-05-22 16:04:24 -0700

    C:/Users/sanchez/AppData/Local/Continuum/anaconda3/Scripts/activate

--- {{cookiecutter.project_slug}}/notebooks/tools.py
+++ {{cookiecutter.project_slug}}/notebooks/tools.py
@@ -6,7 +6,7 @@ def tree(directory):
     """
     directory = Path(directory)
     print(f'+ {directory}')
-    for path in sorted(directory.rglob('*')):
+    for path in sorted(directory.rglob('[!.]*')):
         depth = len(path.relative_to(directory).parts)
         spacer = '    ' * depth
         print(f'{spacer}+ {path.name}')
